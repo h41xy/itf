a54b3810fe0c6885753a46b097a9517ba8d62365
uwo <uwo@localhost.localdomain>
2015-02-04 13:39:01 +0100

    Michl steigt ein

--- snippets/fdisk.bash
+++ snippets/fdisk.bash
@@ -1,4 +1,3 @@
-
 Disk image.dd: 3221 MB, 3221225472 bytes
 255 heads, 63 sectors/track, 391 cylinders, total 6291456 sectors
 Units = sectors of 1 * 512 = 512 bytes
@@ -6,8 +5,8 @@ Sector size (logical/physical): 512 bytes / 512 bytes
 I/O size (minimum/optimal): 512 bytes / 512 bytes
 Disk identifier: 0x0009d1b1
 
-   Device Boot      Start         End      Blocks   Id  System
+Device    Boot      Start         End      Blocks   Id  System
 image.dd1   *           0     3518171     1759086   83  Linux
-image.dd2         3518235     4305419      393592+  82  Linux swap / Solaris
+image.dd2         3518235     4305419      393592+  82  Linux swap
 image.dd3         4305420     6281414      987997+   5  Extended
 image.dd5         4305483     6281414      987966   83  Linux
